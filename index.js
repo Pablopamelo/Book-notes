93dcac7f998db876d6340e4271232daedd757096
Pablopamelo <pawelgrzegorczyk320@gmail.com>
2025-08-01 17:55:46 +0200

    changed some forms and added deleting and editing

--- index.js
+++ index.js
@@ -28,50 +28,105 @@ let currentUserId = 1;
 
 app.get("/", async (req, res) => { 
 	try{
-		const response = await db.query("SELECT title, rating, cover FROM books AS b JOIN users AS u ON b.user_id = u.id WHERE u.id = $1",[currentUserId]); // Gets books data from database for a current user 
+		const response = await db.query("SELECT b.id, b.title, b.rating, b.cover FROM books AS b JOIN users AS u ON b.user_id = u.id WHERE u.id = $1",[currentUserId]); // Gets books data from database for a current user 
 		books = response.rows;
-		console.log(books);
 	}catch(error){
 		console.log(error);
 	}
 	res.render("index.ejs", {books: books});
 });
 
-app.get("/new/:id", (req,res) => {
-	const id = req.params.id;
+app.post("/new/:id", (req,res) => {
+	const id = parseInt(req.params.id);
 	if(id !== 0){
 		const book = books.find((book) => id === book.id);
+		console.log(book.rating);
 		res.render("addBook.ejs",{book});
 	}else{
-		res.render("addBook.ejs");
+		res.render("addBook.ejs",{book: null});
 	}
 });
 
 app.post("/add", async (req,res) => {
 	try{
 		const title = req.body.title;
-		const stars = req.body.stars;
-		await db.query("INSERT INTO books(title,rating,cover,user_id) VALUES($1,$2,$3,$4)",[title, stars, "coming soon", currentUserId]);
+		const rating = req.body.rating;
+		let response;
+
+		try{
+			response = await axios.get("https://openlibrary.org/search.json",{
+				params:{
+					q: title,
+				},
+			});
+		}catch(error){
+			console.log(error);
+		}
+		
+		const bookData = response.data.docs?.[0];
+		const cover_olid = bookData.cover_edition_key;	
+		let cover_url;
+
+		if(cover_olid === '' || cover_olid == "undefined"){
+			cover_url = "https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png";
+		}else{
+			cover_url = `https://covers.openlibrary.org/b/olid/${cover_olid}.jpg`;
+		}
+
+		await db.query("INSERT INTO books(title,rating,cover,user_id) VALUES($1,$2,$3,$4)",
+		[title, rating, cover_url, currentUserId]);
+
 	}catch(error){
 		console.log(error);
 	}
 	res.redirect("/");
 });
 
-app.post("/edit/:id", (req,res) => {
+app.post("/edit/:id", async (req,res) => {
 	try{
+		const id = req.params.id;
+		const title = req.body.title;
+		const rating = req.body.rating;
 
+		let response;
+
+		try{
+			response = await axios.get("https://openlibrary.org/search.json",{
+				params:{
+					q: title,
+				},
+			});
+		}catch(error){
+			console.log(error);
+		}
+		
+		const bookData = response.data.docs?.[0];
+		const cover_olid = bookData.cover_edition_key;	
+		let cover_url;
+
+		if(cover_olid == '' || cover_olid == "undefined"){
+			cover_url = "https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png";
+		}else{
+			cover_url = `https://covers.openlibrary.org/b/olid/${cover_olid}.jpg`;
+		}
+
+		await db.query(`UPDATE books SET title = $1, rating = $2, cover = $3 WHERE id = $4`,[title, rating, cover_url, id])
+		
 	}catch(error){
 		console.log(error);
 	}
+	res.redirect("/");
 });
 
-app.post("/delete/:id", (req,res) => {
+app.post("/delete/:id", async (req,res) => {
 	try{
-
+		const id = req.params.id;
+		db.query("DELETE FROM books WHERE id = $1",
+		[id]);
 	}catch(error){
 		console.log(error);
 	}
+	res.redirect("/");
 });
 
 app.listen(port, () => {
